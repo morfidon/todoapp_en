a2dcba377fb616c5aa5f12441c9cc3e497560a6a
morfidon <armiksos@gmail.com>
2023-05-30 14:05:43 +0200

    Creating views

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome2');
+});
+Route::get('/second', function () {
+    return view('secondview');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
 });
