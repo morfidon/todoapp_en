a416243daa150c9f25fcfebb78c6bddf6875f9f1
morfidon <armiksos@gmail.com>
2023-07-06 11:52:03 +0200

    Importing resources

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield("title")</title>
+    <link rel="stylesheet" href="{{ asset("css/style.css")  }}">
+    <script src="{{ asset("js/script.js") }}"></script>
 </head>
 <body>
 
@@ -18,5 +20,6 @@
         <a href="{{ route("blog.index") }}">Blog</a>
         <a href="{{ route("home.index") }}">Home</a>
     </nav>
+    <img src="{{ asset("images/cat.jpg") }}" alt="" srcset="">
 </body>
 </html>
